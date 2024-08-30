75726b65b09b0968d9c306b7364a6d475ca04ebb
salim45d <salim.agus@live.com>
2024-08-30 19:10:05 +0800

    feat: Prime Number Code

--- b-prime-number.ts
+++ b-prime-number.ts
@@ -16,32 +16,32 @@ for(let i: number = 1; i <= number; i++){
     // Jika angka looping(i) sama dengan 1 maka bukan bilangan prima
     if(i === 1) {
 
-        resultPrimeNumber = `${i} is not Prime Number`
+        resultPrimeNumber = `${i} is not Prime Number`;
 
     // Dan jika angka looping(i) habis dibagi 2 sama dengan nol atau Bil. Genap dan bukan sama dengan 2 maka bukan bilangan prima
     } else if(i % 2 === 0 && i !== 2) {
 
-        resultPrimeNumber = `${i} is not Prime Number`
+        resultPrimeNumber = `${i} is not Prime Number`;
         
     // Dan jika angka looping(i) habis dibagi 3 sama dengan nol dan bukan sama dengan 3 maka bukan bilangan prima
     } else if (i % 3 === 0 && i !==3){
 
-        resultPrimeNumber = `${i} is not Prime Number`
+        resultPrimeNumber = `${i} is not Prime Number`;
         
     // Dan jika angka looping(i) habis dibagi 5 sama dengan nol dan bukan sama dengan 5 maka bukan bilangan prima
     } else if (i % 5 === 0 && i !==5){
 
-        resultPrimeNumber = `${i} is not Prime Number`
+        resultPrimeNumber = `${i} is not Prime Number`;
         
     // Dan jika angka looping(i) habis dibagi 7 sama dengan nol dan bukan sama dengan 7 maka bukan bilangan prima
     } else if (i % 7 === 0 && i !==7){
 
-        resultPrimeNumber = `${i} is not Prime Number`
+        resultPrimeNumber = `${i} is not Prime Number`;
 
     // Dan jika selain angka looping(i) yang disebutkan diatas maka itu adalah bilangan prima
     } else {
 
-        resultPrimeNumber = `${i} is Prime Number`
+        resultPrimeNumber = `${i} is Prime Number`;
        
     }
 
